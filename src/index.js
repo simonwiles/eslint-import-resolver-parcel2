29939712875a09eadc80bef7027c30ee9ad95e5c
Simon Wiles <simonjwiles@gmail.com>
2020-11-15 16:21:34 -0800

    Linting

--- src/index.js
+++ src/index.js
@@ -1,16 +1,16 @@
-import { posix as path } from 'path';
-import fs from 'fs';
-import { isCore, sync as resolveSync } from 'resolve';
+import { posix as path } from "path";
+import fs from "fs";
+import { isCore, sync as resolveSync } from "resolve";
 
-const defaultExtensions = ['.js', '.jsx'];
+const defaultExtensions = [".js", ".jsx"];
 export const interfaceVersion = 2;
 
 function findAliases() {
   let currentPath = path.resolve();
-  let packagePath = path.resolve(currentPath, 'package.json');
+  let packagePath = path.resolve(currentPath, "package.json");
   while (!fs.existsSync(packagePath)) {
-    currentPath = path.resolve(currentPath, '..');
-    packagePath = path.resolve(currentPath, 'package.json');
+    currentPath = path.resolve(currentPath, "..");
+    packagePath = path.resolve(currentPath, "package.json");
   }
   const packageJson = fs.readFileSync(packagePath);
   return JSON.parse(packageJson).alias || {};
@@ -20,14 +20,14 @@ function resolvePackageLevel(source, file, rootDirectory) {
   let packageDirectory = path.dirname(file);
 
   for (;;) {
-    if (fs.existsSync(path.resolve(packageDirectory, 'node_modules'))) {
+    if (fs.existsSync(path.resolve(packageDirectory, "node_modules"))) {
       // found node_modules
       break;
     } else if (path.parse(packageDirectory).root === packageDirectory) {
       // reached the drive root, just return null
       return null;
     }
-    packageDirectory = path.resolve(packageDirectory, '..');
+    packageDirectory = path.resolve(packageDirectory, "..");
   }
 
   return path.join(
@@ -43,9 +43,9 @@ function resolvePackageLevel(source, file, rootDirectory) {
 export function resolve(source, file, possibleConfig = {}) {
   if (isCore(source)) return { found: true, path: null };
 
-  const config = { rootDir: '', extensions: [], ...possibleConfig };
+  const config = { rootDir: "", extensions: [], ...possibleConfig };
 
-  const [startSource] = source.split('/');
+  const [startSource] = source.split("/");
   const aliases = findAliases();
   const foundAlias = Object.keys(aliases).find(
     (alias) => alias === startSource,
@@ -56,15 +56,15 @@ export function resolve(source, file, possibleConfig = {}) {
   const rootDirectory = path.resolve(config.rootDir);
 
   switch (newSource[0]) {
-    case '.':
+    case ".":
       newSource = path.resolve(path.dirname(file), newSource);
       break;
 
-    case '~':
+    case "~":
       newSource = resolvePackageLevel(newSource, file, rootDirectory);
       break;
 
-    case '/':
+    case "/":
       newSource = path.join(rootDirectory, newSource);
       break;
 
